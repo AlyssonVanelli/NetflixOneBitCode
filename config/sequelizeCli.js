4c7df0a4b592ee7b6e36ac11e8630909d622f0c7
Alysson <aharon_curitiba@hotmail.com>
2022-07-18 19:58:40 -0300

    estrutura base inicial

---
+++ config/sequelizeCli.js
@@ -0,0 +1,10 @@
+module.exports = {
+    developement: {
+        dialect: 'postgres',
+        host: 'localhost',
+        port: '5432',
+        database: 'onebitflix',
+        username: 'admin',
+        password: 'onebitflix'
+    }
+}
